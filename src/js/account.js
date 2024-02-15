bd2c96abe06851bdbc183becf44782ca2db4d980
Robert <robert@robert.univrs>
2024-02-15 13:54:03 +0200

    Allow user logout

--- src/js/account.js
+++ src/js/account.js
@@ -1,8 +1,27 @@
 import accountPartial from '../partials/account.html';
 
+const logout = (event) => {
+	if (!event.target.classList.contains('sign-out')) {
+		return;
+	}
+	event.preventDefault();
+	axios.post('https://auth.origin.univrs.cloud/api/logout', null, { withCredentials: true })
+		.then(() => {
+
+		})
+		.catch((error) => {
+
+		})
+		.then(() => {
+			location.reload();
+		});
+};
+
 const render = () => {
 	const template = _.template(accountPartial);
 	morphdom(document.querySelector('#account'), template());
 };
+
+document.querySelector('header').addEventListener('click', logout);
 
 render();
