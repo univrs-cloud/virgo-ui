ac04422dada36b4b55dcfe7d95c1bbdfa0d7d7a4
Robert <robert@robert.univrs>
2024-03-01 10:19:17 +0200

    Handle unconfigured auth proxy

--- src/js/account.js
+++ src/js/account.js
@@ -3,10 +3,10 @@ import accountPartial from '../partials/account.html';
 let header = document.querySelector('header');
 let container = document.querySelector('#account');
 let auth = _.find(proxies, { forwardPort: 9091 });
-let authDomain = _.first(auth.domainNames);
+let authDomain = _.first(auth?.domainNames);
 
 const logout = (event) => {
-	if (!event.target.classList.contains('sign-out')) {
+	if (!event.target.classList.contains('sign-out') || !authDomain) {
 		return;
 	}
 	
