386dcd346651b4d3c6aa0da883263e243fd3eed2
Robert <robert@robert.univrs>
2024-05-15 21:45:52 +0300

    Show shares empty state

--- src/js/shares.js
+++ src/js/shares.js
@@ -1,8 +1,10 @@
 import sharesPartial from '../partials/shares.html';
+import sharesEmptyPartial from '../partials/shares_empty.html';
 import sharePartial from '../partials/share.html';
 import * as shareService from './services/share';
 
 const sharesTemplate = _.template(sharesPartial);
+const sharesEmptyTemplate = _.template(sharesEmptyPartial);
 const shareTemplate = _.template(sharePartial);
 let container = document.querySelector('#shares');
 
@@ -14,9 +16,13 @@ const render = (state) => {
 	let template = document.createElement('template');
 	template.innerHTML = sharesTemplate();
 	let shares = template.content.querySelector('.col');
-	_.each(state.shares, (entity) => {
-		shares.insertAdjacentHTML('beforeend', shareTemplate({ entity }));
-	});
+	if (_.isEmpty(state.shares)) {
+		shares.insertAdjacentHTML('beforeend', sharesEmptyTemplate());
+	} else {
+		_.each(state.shares, (entity) => {
+			shares.insertAdjacentHTML('beforeend', shareTemplate({ entity }));
+		});
+	}
 	morphdom(
 		container,
 		`<div>${template.innerHTML}</div>`,
