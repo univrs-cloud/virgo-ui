4d2cbf302176a7fb0cb432003242b2efc5f6e977
Robert <robert.gurde@gmail.com>
2024-10-07 18:41:02 +0300

    improve morphing

--- src/js/common/morphdom.js
+++ src/js/common/morphdom.js
@@ -1,30 +1,37 @@
 import mdom from 'morphdom';
 
 const morphdom = (fromNode, toNode, options) => {
+	let newOptions = options || {};
 	let onBeforeElUpdated = options?.onBeforeElUpdated;
-	options = options || {};
-	options.onBeforeElUpdated = (fromEl, toEl) => {
+	newOptions.onBeforeElUpdated = (fromEl, toEl) => {
 		if (onBeforeElUpdated) {
 			let result = onBeforeElUpdated(fromEl, toEl);
 			if (result === false) {
 				return false;
 			}
 		}
-
-		if (fromEl.hasAttribute('data-bs-toggle') && (fromEl.getAttribute('data-bs-toggle') === 'tooltip' || fromEl.getAttribute('data-bs-toggle') === 'popover' || fromEl.getAttribute('data-bs-toggle') === 'tab')) {
-			morphdom(fromEl, toEl, { childrenOnly: true });
+		
+		if (fromEl.getAttribute('data-bs-toggle') === 'tab') {
+			mdom(fromEl, toEl, { childrenOnly: true });
 			return false;
 		}
+		
+		if (fromEl.getAttribute('data-bs-toggle') === 'tooltip') {
+			mdom(fromEl, toEl, { childrenOnly: true });
+			if (fromEl.hasAttribute('data-bs-original-title')) {
+				fromEl.dataset.bsOriginalTitle = toEl.getAttribute('title') ?? '';
+			}
 
-		if (fromEl.classList.contains('dropdown-menu')) {
-			morphdom(fromEl, toEl, { childrenOnly: true });
+			return false;
 		}
-		if ((fromEl.getAttribute('data-bs-toggle') === 'dropdown' || fromEl.classList.contains('dropdown-menu')) && fromEl.classList.contains('show')) {
+		
+		if ((fromEl.classList.contains('dropdown-toggle') || fromEl.classList.contains('dropdown-menu')) && fromEl.classList.contains('show')) {
+			morphdom(fromEl, toEl, { childrenOnly: true });
 			return false;
 		}
 	};
 
-	return mdom(fromNode, toNode, options);
+	return mdom(fromNode, toNode, newOptions);
 };
 
 export {
