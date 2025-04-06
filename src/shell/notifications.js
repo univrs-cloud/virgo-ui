35c903291af61acbc4ed9276c95f53a20771bdb7
Robert <robert.gurde@gmail.com>
2025-04-07 01:56:00 +0300

    Improve styling

--- src/shell/notifications.js
+++ src/shell/notifications.js
@@ -16,8 +16,8 @@ const render = (state) => {
 
 const init = () => {
 	render({ notifications: [
-		{ type: 'info', title: 'Data integrity scan', message: 'Scrub repaired 0B in 00:21:04 with 0 errors on Fri Nov 1 00:24:33 2024.', createdOn: '2024-11-01T00:24:33.000Z' },
-		{ type: 'warning', title: 'Drive temperature warning', message: 'Drive nvme0 is experiencing temperatures (75°C) above typical operating levels.', createdOn: '2024-10-30T12:11:26.000Z' }
+		// { type: 'info', title: 'Data integrity scan', message: 'Scrub repaired 0B in 00:21:04 with 0 errors on Fri Nov 1 00:24:33 2024.', createdOn: '2024-11-01T00:24:33.000Z' },
+		// { type: 'warning', title: 'Drive temperature warning', message: 'Drive nvme0 is experiencing temperatures (75°C) above typical operating levels.', createdOn: '2024-10-30T12:11:26.000Z' }
 	] });
 };
 
