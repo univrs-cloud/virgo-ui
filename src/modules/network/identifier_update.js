c1752a69b256a637ca30d66cf360183f1fe3df6d
Robert <robert.gurde@gmail.com>
2025-11-27 02:42:11 +0200

    Use form web component

--- src/modules/network/identifier_update.js
+++ src/modules/network/identifier_update.js
@@ -4,54 +4,17 @@ import validator from 'validator';
 
 document.querySelector('body').insertAdjacentHTML('beforeend', identifierModalPartial);
 
-const form = document.querySelector('#network-identifier');
-
-const validateHostname = () => {
-	const input = form.querySelector('.hostname');
-	const value = input.value;
-	if (validator.isEmpty(value.toString())) {
-		input.error = `Can't be empty`;
-		return;
-	}
-	input.error = ``;
-};
-
-const validateDomainName = () => {
-	const input = form.querySelector('.domain-name');
-	const value = input.value;
-	if (validator.isEmpty(value.toString())) {
-		input.error = `Can't be empty`;
-		return;
-	}
-	input.error = ``;
-};
-
-const validateForm = () => {
-	validateHostname();
-	validateDomainName();
-};
-
-const isFormValid = () => {
-	validateForm();
-	return _.isEmpty(form.querySelectorAll('.is-invalid'));
-};
+const modal = document.querySelector('#network-identifier');
+const form = modal.closest('u-form');
 
 const updateIdentifier = (event) => {
-	event.preventDefault();
-	if (!isFormValid()) {
-		return;
-	}
-
 	const form = event.target;
 	const buttons = form.querySelectorAll('button');
 	_.each(buttons, (button) => { button.disabled = true; });
 
-	let config = {
-		hostname: form.querySelector('.hostname').value,
-		domainName: form.querySelector('.domain-name').value
-	};
+	let config = form.getData();
 	networkService.updateHostIdentifier(config);
-	bootstrap.Modal.getInstance(form.closest('.modal'))?.hide();
+	bootstrap.Modal.getInstance(modal)?.hide();
 };
 
 const render = (event) => {
@@ -63,14 +26,22 @@ const render = (event) => {
 const restore = (event) => {
 	form.reset();
 	_.each(form.querySelectorAll('button'), (button) => { button.disabled = false });
-	_.each(form.querySelectorAll('.form-floating'), (field) => {
-		field.querySelector('input')?.classList?.remove('is-invalid', 'is-valid');
-		field.querySelector('.invalid-feedback').innerHTML = '';
-	});
 };
 
-form.querySelector('.hostname').addEventListener('input', validateHostname);
-form.querySelector('.domain-name').addEventListener('input', validateDomainName);
-form.addEventListener('submit', updateIdentifier);
+form.validation = [
+	{
+		selector: '.hostname',
+		rules: {
+			isEmpty: `Can't be empty`
+		}
+	},
+	{
+		selector: '.domain-name',
+		rules: {
+			isEmpty: `Can't be empty`
+		}
+	}
+];
+form.addEventListener('valid', updateIdentifier);
 form.addEventListener('show.bs.modal', render);
 form.addEventListener('hidden.bs.modal', restore);
