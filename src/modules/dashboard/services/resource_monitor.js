7d403fa0218dadca264b512c90369130f4be038e
Robert <robert.gurde@gmail.com>
2025-12-03 14:02:55 +0200

    Show more accurate RAM usage

--- src/modules/dashboard/services/resource_monitor.js
+++ src/modules/dashboard/services/resource_monitor.js
@@ -3,6 +3,17 @@ import Host from 'stores/host';
 let callbackCollection = [];
 
 const handleSubscription = (properties) => {
+	if (properties.memory) {
+		properties.memory.unreclaimable = {};
+		properties.memory.unreclaimable.value = properties.memory.total - properties.memory.available;
+		properties.memory.unreclaimable.percent = (properties.memory.unreclaimable.value / properties.memory.total) * 100;
+		properties.memory.reclaimable = {};
+		properties.memory.reclaimable.value = properties.memory.available - properties.memory.free;
+		properties.memory.reclaimable.percent = (properties.memory.reclaimable.value / properties.memory.total) * 100;
+		properties.memory.capacity = {};
+		properties.memory.capacity.value = properties.memory.reclaimable.value + properties.memory.unreclaimable.value;
+		properties.memory.capacity.percent = properties.memory.reclaimable.percent + properties.memory.unreclaimable.percent;
+	}
 	_.each(callbackCollection, (callback) => {
 		callback(properties);
 	});
