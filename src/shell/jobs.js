d0ce52203ecc6fe0ac9ecd675d2570081b70aeda
Robert <robert.gurde@gmail.com>
2025-12-09 23:22:57 +0200

    Use notifier component

--- src/shell/jobs.js
+++ src/shell/jobs.js
@@ -1,8 +1,9 @@
-import toastPartial from 'shell/partials/toast.html';
+import jobProgressPartial from 'shell/partials/job_progress.html';
 import * as jobService from 'shell/services/job';
 
-const toastTemplate = _.template(toastPartial);
-const shownJobIds = new Set();
+const jobProgressTemplate = _.template(jobProgressPartial);
+const acknowledgedJobIds = new Set();
+const toasts = {};
 
 const render = (state) => {
 	_.each(state.jobs, (job) => {
@@ -10,41 +11,70 @@ const render = (state) => {
 			return;
 		}
 		
-		if (shownJobIds.has(job.id)) {
+		if (acknowledgedJobIds.has(job.id)) {
 			return;
 		}
 		
 		if (job.progress.state !== 'active') {
-			shownJobIds.add(job.id);
+			acknowledgedJobIds.add(job.id);
 		}
 
-		const hasToast = !_.isNull(document.querySelector(`#toast-${job.id}`));
-		const toast = toastTemplate({ job, hasToast, moment, prettyBytes });
-		if (hasToast) {
-			if (!document.querySelector(`#toast-${job.id}`).classList.contains('active') || (document.querySelector(`#toast-${job.id}`).classList.contains('active') && job.progress.state === 'active')) {
-				morphdom(
-					document.querySelector(`#toast-${job.id}`),
-					toast
-				);
-				return;
-			}
-			if (document.querySelector(`#toast-${job.id}`).classList.contains('active') && job.progress.state !== 'active') {
-				const oldToast = bootstrap.Toast.getOrCreateInstance(document.querySelector(`#toast-${job.id}`));
-				oldToast.hide();
-				document.querySelector('.toast-container').insertAdjacentHTML('beforeend', toast);
-				const newToast = new bootstrap.Toast(document.querySelector('.toast-container .toast:last-of-type'));
-				newToast.show();
-			}
+		const hasToast = !_.isUndefined(toasts[job.id]);
+		const title =  job.progress.message;
+		let type = 'info';
+		let dismissible = false;
+		let duration = 0;
+		if (job.progress.state === 'completed') {
+			type = 'success';
+			dismissible = true;
+			duration = 5000;
+		}
+		if (job.progress.state === 'failed') {
+			type = 'error';
+			dismissible = true;
+		}
+		let message = '';
+		message += jobProgressTemplate({ job });
+		message += `<div>${(job.progress.state === 'failed' ? job.failedReason : '')}</div>`;
+		if (!hasToast) {
+			toasts[job.id] = notifier.add({ title, message, type, dismissible, duration });
 		} else {
-			document.querySelector('.toast-container').insertAdjacentHTML('beforeend', toast);
-			const newToast = new bootstrap.Toast(document.querySelector('.toast-container .toast:last-of-type'));
-			newToast.show();
+			const toast = toasts[job.id];
+			toast.update({ title, message, type, dismissible, duration });
 		}
 	});
 };
 
 const init = () => {
 	jobService.subscribe([render]);
+
+	// const ref = window.notifier.add({
+	// 	type: 'info',
+	// 	title: 'Processing...',
+	// 	message: 'Starting operation (0%)',
+	// 	duration: 0,
+	// 	dismissible: false
+	//   });
+	  
+	//   let progress = 0;
+	//   const interval = setInterval(() => {
+	// 	progress += 20;
+		
+	// 	if (progress < 100) {
+	// 	  ref.update({
+	// 		message: `Processing... (${progress}%)`
+	// 	  });
+	// 	} else {
+	// 	  ref.update({
+	// 		type: 'success',
+	// 		title: 'Complete!',
+	// 		message: 'Operation finished successfully.',
+	// 		dismissible: true,
+	// 		duration: 3000
+	// 	  });
+	// 	  clearInterval(interval);
+	// 	}
+	//   }, 800);
 };
 
 export {
