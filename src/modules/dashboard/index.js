e2ad833ffb8e0a036a0f663323efa88ba0291f20
Robert <robert.gurde@gmail.com>
2026-04-28 00:08:01 +0300

    Remove metrics module

--- src/modules/dashboard/index.js
+++ src/modules/dashboard/index.js
@@ -3,7 +3,6 @@ import modulePartial from 'modules/dashboard/partials/index.html';
 const moduleTemplate = _.template(modulePartial);
 document.querySelector('main .modules').insertAdjacentHTML('beforeend', moduleTemplate());
 
-import('modules/dashboard/metrics');
 import('modules/dashboard/resources_monitor');
 import('modules/dashboard/apps_bookmarks');
 import('modules/dashboard/shares');
